a9c8feb22e82c3a3678d50c30f41744bd8565af6
firman <firmaneymar@gmail.com>
2025-08-27 22:15:13 +0800

    feat sh : add data_viewer file to view render database

--- allpage.py
+++ allpage.py
@@ -5,7 +5,8 @@ homepage_page = st.Page("pages/homepage.py", title="Homepage", icon="🏠")
 pengesahan_page = st.Page("pages/pengesahan.py", title="Pengesahan", icon="📄")
 berjaya_page = st.Page("pages/berjaya.py", title="Berjaya", icon="🟢")
 gagal_page = st.Page("pages/gagal.py", title="Gagal", icon="🔴")
+data_viewer_page = st.Page("pages/data_viewer.py", title="Data Viewer", icon="📊")
 
 # Navigation controller
-pg = st.navigation([homepage_page, pengesahan_page, berjaya_page, gagal_page])
+pg = st.navigation([data_viewer_page, homepage_page, pengesahan_page, berjaya_page, gagal_page])
 pg.run()
